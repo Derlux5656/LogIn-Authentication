3a214ae1a328e7860fb5d2e69fc6f87eb10232c9
Dimitri Verushi <drverushi@gmail.com>
2021-11-23 18:16:10 +0100

    Third Commit

--- app.js
+++ app.js
@@ -12,11 +12,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use( "/Public", express.static('Public'));      
 
-let PutYourEmailAgain = [];
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/LogInAuthentication');
+
+const PersonalData = mongoose.model('userdata', { 
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+
+const PutYourEmailAgain = [];
+
+const FirstUser = new PersonalData({
+  email: "drverushi@gmail.com",
+  password: "taqi",
+})
+
 
 
 app.get("/", (req,res)=>{
- res.render('LogIn_Page')
+
+ res.render('LogIn_Page');
 
 })
 
@@ -29,21 +52,33 @@ app.get("/signup", (req,res)=>{
 
 })
 
+
+
+
 app.post("/signup", (req,res)=>{
     // User Inputs
   const email= req.body.SignUpEmail;
   const comfirmedEmail = req.body.ComfirmedEmail;
   const password= req.body.SignUpPassword;
 
-  if(email === comfirmedEmail){
-    console.log("Email comfirmed with success")
-    res.redirect("/")
-  } else{
-    console.log("Please put your email again");
-    PutYourEmailAgain.push("Your email dosen't match with the confirmed email");
-    res.redirect("/signup")
-  }
 
+  if(email === comfirmedEmail){
+    console.log("Email comfirmed with success");
+    const User = new PersonalData({
+       email: email,
+       password: password 
+    })
+    PersonalData.insertMany[User];
+
+    res.redirect("/");
+  } else{
+
+    console.log("Please put your email again");
+    if(PutYourEmailAgain.length == 0){
+    PutYourEmailAgain.push("Please put your email again");
+    }
+    res.redirect('/signup')
+  }
 })
 
 
